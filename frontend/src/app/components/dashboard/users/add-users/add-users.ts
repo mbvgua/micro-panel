cd15241b76fb1a3ba8ef28fa23b113ce79c4af0c
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-07-17 16:27:38 +0300

    feat(frontend/dashboard): work on the users & loans
    - build the users and loans nested components to allow creationg and
      getting of the users from the database, from the backend.
    
    WINS: hehee... soo much validation. Now onto the HTTPClient

--- frontend/src/app/components/dashboard/users/add-users/add-users.ts
+++ frontend/src/app/components/dashboard/users/add-users/add-users.ts
@@ -1,11 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-users',
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './add-users.html',
-  styleUrl: './add-users.scss'
+  styleUrl: './add-users.scss',
 })
 export class AddUsers {
 
+  // create the reactive form
+  addUserForm = new FormGroup({
+    microfinance_id: new FormControl('', Validators.required),
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone_number: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+    ]),
+    password: new FormControl('', Validators.required),
+  });
+
+
+  addUser(){
+    console.log(this.addUserForm.value)
+  }
 }
