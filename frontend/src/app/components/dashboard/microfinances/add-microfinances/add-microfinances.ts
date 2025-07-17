731da3fe6593aac34f428031efd597c1727c9a73
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-07-17 09:35:04 +0300

    feat(microfinances): CRUD pages for get and add microfinances
    - get the add-users and get-users pages templates. This will serve as a
      temaplate for styling the other admin modules.
    
    - its worth noting that there was a lot of repetitive code, moreso in
      the validation, and though beautiful and simplistic when it works, I
      wonder if there is a cleaner way to write that.
    
    TODO: look in cleaner ways of doing form validation

--- frontend/src/app/components/dashboard/microfinances/add-microfinances/add-microfinances.ts
+++ frontend/src/app/components/dashboard/microfinances/add-microfinances/add-microfinances.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-add-microfinances',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-microfinances.html',
-  styleUrl: './add-microfinances.scss'
+  styleUrl: './add-microfinances.scss',
 })
 export class AddMicrofinances {
+  // create form group instance
+  addMicrofinanceForm = new FormGroup({
+    reg_number: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(150),
+    ]),
+    name: new FormControl('', [Validators.required, Validators.maxLength(60)]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    phone_number: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+    ]),
+    location: new FormControl('', Validators.required),
+  });
 
+  // add microfinance on submit
+  addMicrofinance() {
+    console.log(this.addMicrofinanceForm.value);
+  }
 }
